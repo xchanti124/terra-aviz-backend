32cc1f78757ac5dc0e4cccb4c29f9b112392f68a
Dominik Kropp <dkropp@outlook.de>
2023-01-10 13:35:11 +0100

    Implement location routes

---
+++ src/routes/locationRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getLocations,
+  setLocation,
+  updateLocation,
+  deleteLocation,
+} = require("../controllers/locationController");
+
+router.route("/").get(getLocations).post(setLocation);
+
+router.route("/:id").put(updateLocation).delete(deleteLocation);
+
+module.exports = router;
