d44758bdc2a6761bf2bad6aff548dd3344268593
Dominik Kropp <dkropp@outlook.de>
2023-01-23 15:41:32 +0100

    Quick Fix set likes to 0 from backend

--- src/controllers/locationController.js
+++ src/controllers/locationController.js
@@ -18,13 +18,13 @@ likes
 gAPI
 comments
 imageLink
-hashtagArray
+hashtags
 category
 */
 const setLocation = asyncHandler(async (req, res) => {
   const location = await Location.create({
     description: req.body.description,
-    likes: req.body.likes, // should be set here to zero in future and not from the front end.
+    likes: 0,
     gAPI: req.body.gAPI, // don't have a clue yet how it will work
     comments: req.body.comments,
     imageLink: req.body.imageLink,
