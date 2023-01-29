622312af505f113b1b9a2faf4cf3dc19d101138b
Chantal Ariu <chantalariu227@gmail.com>
2023-01-29 20:54:21 +0100

    Remove placeholder endpoints

--- src/server.js
+++ src/server.js
@@ -28,28 +28,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 // using Errorhandler
 app.use(errorHandler);
 
-// GET -> /
-// This is the root endpoint for the API
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// POST -> /posts
-// This endpoint returns all posts
-app.post("/posts", (req, res) => {
-  // todo: implement it after setting up the database,
-  // currently returns fake data
-  const fakePosts = [];
-  for (let i = 0; i < 15; i++) {
-    fakePosts.push({
-      id: i,
-      content: "some content " + i,
-      likes: 0,
-    });
-  }
-  res.send(JSON.stringify(fakePosts));
-});
-
 // Starts the server on port 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
