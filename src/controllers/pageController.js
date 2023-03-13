cd1f0d7d4c3fddf02da51efa53e80fe9529c8ddd
Dominik Kropp <dkropp@outlook.de>
2023-03-13 17:14:05 +0100

    Implement pagination with search and filter functionality

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -5,13 +5,64 @@ const Location = require("../models/locationModel");
 
 // @desc Get search results
 // @route GET /api/search
-// FOR SEARCH /api/page?pageNumber=0
+// FOR SEARCH /api/page?pageNumber=1
 const pages = asyncHandler(async (req, res) => {
   const locationsPerPage = 10;
-  let pageNumber = req.query.pageNumber-1
+  let pageNumber = req.query.pageNumber-1 || 0;
 
-  const pagination = await Location.find().limit(locationsPerPage).skip(locationsPerPage * pageNumber)
-  res.status(200).json(pagination);
+  if(req.query.search && !req.query.category) {
+    let querySearch = req.query.search;
+    const result = await Location.find({$or:[
+        {description: { $regex: `.*${querySearch}.*`, $options: "i" }}, {title: {$regex: `.*${querySearch}.*`, $options: "i" }}]
+    }).limit(locationsPerPage).skip(locationsPerPage*pageNumber);
+
+    const pageCalc = await Location.find({$or:[
+        {description: { $regex: `.*${querySearch}.*`, $options: "i" }}, {title: {$regex: `.*${querySearch}.*`, $options: "i" }}]
+    });
+
+    let calc = Math.ceil(pageCalc.length / 10);
+    const maxPage = {maxPage: calc};
+    res.status(200).json({result, maxPage});
+
+
+  } else if (!req.query.search && req.query.category) {
+    let queryCategory = {"category" : req.query.category}
+    const result = await Location.find(queryCategory).limit(locationsPerPage).skip(locationsPerPage*pageNumber);
+    const pageCalc = await Location.find(queryCategory);
+
+
+    let calc = Math.ceil(pageCalc.length / 10);
+    const maxPage = {maxPage: calc};
+    res.status(200).json({result, maxPage});
+
+
+  } else if (req.query.search && req.query.category) {
+    let queryCategory = {"category" : req.query.category}
+    let querySearch = req.query.search;
+    const result = await Location.find({$or:[
+        {description: { $regex: `.*${querySearch}.*`, $options: "i" }}, {title: {$regex: `.*${querySearch}.*`, $options: "i" }}], queryCategory
+    }).limit(locationsPerPage).skip(locationsPerPage*pageNumber);
+    const pageCalc = await Location.find({$or:[
+        {description: { $regex: `.*${querySearch}.*`, $options: "i" }}, {title: {$regex: `.*${querySearch}.*`, $options: "i" }}], queryCategory
+    });
+
+    let calc = Math.ceil(pageCalc.length / 10);
+    const maxPage = {maxPage: calc};
+    res.status(200).json({result, maxPage});
+
+
+  } else {
+    const result = await Location.find().limit(locationsPerPage).skip(locationsPerPage * pageNumber)
+    const pageCalc = await Location.find()
+
+
+    let calc = Math.ceil(pageCalc.length / 10);
+    const maxPage = {maxPage: calc};
+    res.status(200).json({result, maxPage});
+
+  }
+
+
 
 
 
