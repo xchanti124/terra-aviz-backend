bcf88bd23417b64ce04a988baa5a1a5710779f76
Chantal Ariu <chantalariu227@gmail.com>
2023-03-16 21:46:08 +0100

    Fix dotenv initialization

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
